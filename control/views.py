789966dbd26b09a8b6e13fb7b3c18f0986ad4a52
Cornelius Hoffmann <coding@hoffmn.de>
2020-09-04 21:23:46 +0200

    Remove outdated view

--- control/views.py
+++ control/views.py
@@ -1,19 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.views.generic import DetailView
 from django_tables2 import SingleTableView
 from .models import Host
 from .tables import HostTable
 from .ipmi import rmcp
 
-def index(request):
-    hostData = {}
-    for host in Host.objects.all():
-        hostData[host] = rmcp.chassisStatus(host.connection(), host.port, host.cred.username, host.cred.password)
-    template = loader.get_template('control/index.html')
-    context = {'hostData': hostData}
-    return render(request, 'control/index.html', context)
 
 class HostView(DetailView):
     model = Host
