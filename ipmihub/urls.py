7dbb323e4e3d4115aefc658d9d93d5a7067c02f5
Cornelius Hoffmann <coding@hoffmn.de>
2020-09-17 21:23:34 +0200

    Let index redirect to control app

--- ipmihub/urls.py
+++ ipmihub/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='control/')),
     path('control/',include('control.urls')),
     path('admin/', admin.site.urls),
 ]
